9e787ccdd747d86eceb683ed1f19205779df1789
Xiangyu Dong <slam.printer.08@icloud.com>
2026-04-08 16:52:07 -0700

    My Prod-Grade learning note Section 3: Azure, GCP, Data Engineering, Vectors, MCP, SageMaker embedding endpoint

--- terraform/2_sagemaker/variables.tf
+++ terraform/2_sagemaker/variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 variable "sagemaker_image_uri" {
   description = "URI of the SageMaker container image"
   type        = string
-  default     = "763104351884.dkr.ecr.us-east-1.amazonaws.com/huggingface-pytorch-inference:1.13.1-transformers4.26.0-cpu-py39-ubuntu20.04"
+  default     = "763104351884.dkr.ecr.us-west-2.amazonaws.com/huggingface-pytorch-inference:1.13.1-transformers4.26.0-cpu-py39-ubuntu20.04"
 }
 
 variable "embedding_model_name" {
